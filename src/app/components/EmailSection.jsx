88f31246bc641982fd98ee0d743ab054455c1404
Anosh-Raza <anoshraxa@gmail.com>
2024-04-23 15:40:04 +0500

    LinkedIn Github Added

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/Anosh-Raza">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/anosh-raza-790259168/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
